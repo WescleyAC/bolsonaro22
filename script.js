3c37e6e05ff57e3d774442bea498c57d10858600
wescley <wescleypbpe@gmail.com>
2022-10-24 12:40:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function nameUser() {
+
+  let vName =  document.getElementById("form-name").value 
+
+  
+
+ document.getElementById("name-campo").innerHTML = vName.toUpperCase()
+
+ 
+
+console.log(vName)
+}
